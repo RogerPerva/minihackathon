6b9bdf7fae3ac122318e8b8e65c9f61b20891d4c
rogerpervaz <ulisescodi@gmail.com>
2025-04-18 16:23:34 -0600

    start

---
+++ src/main/java/com/project/minihackathon/MinihackathonApplication.java
@@ -0,0 +1,13 @@
+package com.project.minihackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinihackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinihackathonApplication.class, args);
+	}
+
+}
