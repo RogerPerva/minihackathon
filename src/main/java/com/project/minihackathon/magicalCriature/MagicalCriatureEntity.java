8b4cccc9e007695488e2dfcd5d41fdb6c6c464d3
rogerpervaz <ulisescodi@gmail.com>
2025-04-18 16:42:05 -0600

    magical creature and speel entities

--- src/main/java/com/project/minihackathon/magicalCriature/MagicalCriatureEntity.java
+++ src/main/java/com/project/minihackathon/magicalCriature/MagicalCriatureEntity.java
@@ -1,4 +1,51 @@
 package com.project.minihackathon.magicalCriature;
 
-public class MagicalCriatureEntity {
-}
+import com.project.minihackathon.enums.CreatureHabit;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDate;
+
+@Entity
+  @Table(name = "magical_creatures")
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  public class MagicalCriatureEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String species;
+
+    @Column(length = 512)
+    private String description;
+
+    @Column(name = "danger_level", nullable = false)
+    private int dangerLevel;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CreatureHabit habitat;
+
+    @Column(name = "is_domesticated", nullable = false)
+    private boolean isDomesticated;
+
+    @Column(name = "discovered_date", nullable = false)
+    private LocalDate discoveredDate;
+  }
+
--- src/main/java/com/project/minihackathon/spell/SpellEntity.java
+++ src/main/java/com/project/minihackathon/spell/SpellEntity.java
@@ -1,4 +1,52 @@
 package com.project.minihackathon.spell;
 
+import com.project.minihackathon.enums.SpellDifficulty;
+import com.project.minihackathon.enums.SpellType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "spells")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SpellEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 1024)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SpellDifficulty difficulty;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SpellType type;
+
+    @Column(name = "requires_wand", nullable = false)
+    private boolean requiresWand;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDate createdAt;
+
 }
